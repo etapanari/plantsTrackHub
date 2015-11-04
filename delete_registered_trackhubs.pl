e783abac4475236999472ace15c86a52b2212129
Electra Tapanari <etapanari@gmail.com>
2015-11-04 14:19:18 +0000

    increment update

--- delete_registered_trackhubs.pl
+++ delete_registered_trackhubs.pl
@@ -1,5 +1,5 @@
 
-# in this cript I connect to the trackHub Registry db and I register (upload) a track hub
+# in this cript I connect to the trackHub Registry db and I delete the given study_id under the given track hub Registry account
 
   use strict ;
   use warnings;
@@ -16,15 +16,17 @@
   my $ua = LWP::UserAgent->new;
 
 # example call:
-#perl delete_registered_trackhubs.pl -username etapanari -password ensemblplants >output
+#perl delete_registered_trackhubs.pl -username tapanari -password testing -study_id SRP002106
 
-  my $username ;
+  my $username ;  # it is etapanari / tapanari
   my $pwd ; # i pass the pwd when calling the pipeline, in the command line  # it is ensemblplants/ testing
+  my $study_id;
 
 
   GetOptions(
      "username=s" => \$username ,
-     "password=s" => \$pwd
+     "password=s" => \$pwd,
+     "study_id=s" => \$study_id
   );
 
 
@@ -35,11 +37,10 @@
   my $request = GET($url) ;
 
   $request->headers->authorization_basic($username, $pwd);
-  # print Dumper $request;
   my $response = $ua->request($request);
   my $auth_token = from_json($response->content)->{auth_token};
   
-  $url = $server . '/api/trackhub';
+  $url = $server . '/api/trackhub/';
   $request = GET($url);
   $request->headers->header(user => $username);
   $request->headers->header(auth_token => $auth_token);
@@ -48,14 +49,19 @@
   my $response_code= $response->code;
   my $counter_of_deleted=0;
 
+
   if($response_code == 200) {
+
     foreach my $trackhub (@{from_json($response->content)}) {
 
+      if($study_id ne "all"){
+         next unless $trackhub->{name} eq $study_id;
+      }
       printf "Got hub %s\n", $trackhub->{name};
 
       foreach my $trackdb (@{$trackhub->{trackdbs}}) {
 
-        printf "TrackDb %s does not have an URI. Skipping...\n", $trackdb->{assembly} and next unless $trackdb->{uri};
+        printf STDERR "TrackDb %s does not have an URI. Skipping...\n", $trackdb->{assembly} and next unless $trackdb->{uri};
         printf "\tDeleting %s trackdb...", $trackdb->{assembly};
         $request = DELETE($trackdb->{uri});
         $request->headers->header(user => $username);
@@ -63,7 +69,7 @@
         $response = $ua->request($request);
         $counter_of_deleted++ if $response->code == 200;
         print "Done (deleted $counter_of_deleted track hubs so far)\n" and next if $response->code == 200;
-        printf "Couldn't delete %s: %d\n", $trackdb->{assembly}, $response->code;
+        printf STDERR "Couldn't delete %s: %d\n", $trackdb->{assembly}, $response->code;
       }
     }
     

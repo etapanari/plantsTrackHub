e0f6d9f227d4b1ce07ed13881ac96ab07ab52910
Electra Tapanari <etapanari@gmail.com>
2016-03-01 15:04:33 +0000

    latest changes

--- JsonResponse.pm
+++ JsonResponse.pm
@@ -10,23 +10,45 @@ my $http = HTTP::Tiny->new();
 
 sub get_Json_response { # it returns the json response given the endpoint as param, it returns an array reference that contains hash references . If response not successful it returns 0
   
-  my $url = shift; # example: "http://plantain:3000/eg/getLibrariesByStudyId/SRP033494";
+  my $url = shift; # example: "http://plantain:3000/json/70/getRunsByStudyId/SRP033494";
 
   my $response = $http->get($url);
+  my $content;
+  my $json;
 
-  if($response->{success} ==1) { # if the response is successful then I get 1 # checks the url to be correct and server to give response
+  my $response_success= eval {$response->{success} };
 
-    my $content=$response->{content};     
-    my $json = decode_json($content); # it returns an array reference 
+  if ($@) {
+
+    print STDERR "Response success (in the module JsonResponse) value is not 1 but: ".$response->{success}." for url $url\n";
+
+    return 0;
+
+  }elsif($response_success ==1) { # if the response is successful then I get 1 # checks the url to be correct and server to give response
+
+    $content=$response->{content};     
+    $json = decode_json($content); # it returns an array reference 
 
     return ($json);
-
-  }else{
-
-    my ($status, $reason) = ($response->{status}, $response->{reason}); #LWP perl library for dealing with http
-    print STDERR "ERROR in: ".__FILE__." line: ".__LINE__ ." Failed for $url! Status code: ${status}. Reason: ${reason}\n";  # if response is successful I get status "200", reason "OK"
-    return 0;
   }
+#   }else{
+# 
+#     for(my $i=1; $i<=10; $i++) {
+#       sleep 5;
+#       $response = $http->get($url);
+# 
+#       if($response->{success} ==1){
+#         $content=$response->{content};     
+#         $json = decode_json($content); # it returns an array reference 
+#         return ($json);
+#       }
+# 
+#     }
+# 
+#     my ($status, $reason) = ($response->{status}, $response->{reason}); #LWP perl library for dealing with http
+#     print STDERR "ERROR in: ".__FILE__." line: ".__LINE__ ." Failed for $url! Status code: ${status}. Reason: ${reason}\n";  # if response is successful I get status "200", reason "OK"
+#     return 0;
+#   }
 
 }
 
